7e37c3a689c229ba750dc10e2eb1ae84db4733a0
lsomohano <lsomohano@avasa.com.mx>
2022-06-02 11:55:26 -0500

    Cambios de base de datos

--- TiendaOnline/TiendaOnline/settings.py
+++ TiendaOnline/TiendaOnline/settings.py
@@ -75,9 +75,18 @@ WSGI_APPLICATION = 'TiendaOnline.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
@@ -112,7 +121,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
